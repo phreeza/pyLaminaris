fbaa7f36443a673549656d2c42decae58c4b545c
Thomas McColgan <thomas.mccolgan@gmail.com>
2014-03-28 14:40:43 +0100

    whitespace

--- dodo.py
+++ dodo.py
@@ -1,1 +1,23 @@
 __author__ = 'mccolgan'
+
+
+def task_simple_binaural_click():
+    """Timecourses of features over long time courses"""
+
+    from bin import simple_binaural_click_calc as c
+    from bin import simple_binaural_click_plot as p
+    import glob
+
+    yield {
+        'name': 'calc',
+        'actions': [(c.run,)],
+        'targets': [""],
+        'file_dep': ["script/psth_isih_from_txt.py"]
+    }
+
+    yield {
+        'name': 'plot',
+        'actions': [(p.run,)],
+        'targets': [""],
+        'file_dep': ["script/psth_isih_from_txt.py"]
+    }
