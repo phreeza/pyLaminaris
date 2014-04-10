36f3ca81204a590682de8be4040e3b9107f6df8b
Thomas McColgan <thomas.mccolgan@gmail.com>
2014-04-10 12:50:25 +0200

    basic parallel version running. need to improve message passing

--- pyLaminaris/parallel.py
+++ pyLaminaris/parallel.py
@@ -1,9 +1,42 @@
 __author__ = 'mccolgan'
 
+import numpy as np
+import populations as pops
+import experiment
+import recording
+from mpi4py import MPI
+from neuron import h
+
 
 class ParallelExperiment:
-    def __init__(self):
-        pass
+    def __init__(self, n=2):
+        self.pc = h.ParallelContext()
+        self.nhost = int(self.pc.nhost())
+        self.rank = int(self.pc.id())
 
-    def run(self):
-        pass
+        s = "mpi4py thinks I am %d of %d, NEURON thinks I am %d of %d\n"
+        cw = MPI.COMM_WORLD
+        print s % (cw.rank, cw.size,
+                   self.pc.id(), self.pc.nhost())
+
+        print "Minion %i of %i reporting for duty" % (self.rank, self.nhost)
+
+        self.sizes = np.array([int(n / self.nhost) for i in range(n)])
+        self.sizes[:(n % self.nhost)] += 1
+
+        self.mysize = self.sizes[self.rank]
+
+    def setup(self):
+        self.exp = experiment.Experiment()
+        self.ipsi_pop = pops.NMNeuronPopulation(side='ipsi', size=self.mysize, record=False)
+        self.contra_pop = pops.NMNeuronPopulation(side='contra', size=self.mysize, record=False)
+        self.electrode = recording.Electrode(location=np.array([0., 0., 0.]))
+        self.ipsi_pop.set_stimulation(stimtype='click', freq=4000.)
+        self.contra_pop.set_stimulation(stimtype='click', freq=4000.)
+        self.exp.add_population(self.ipsi_pop)
+        self.exp.add_population(self.contra_pop)
+        self.exp.add_electrode(self.electrode)
+
+    def run(self, t=20., mode='step'):
+        self.exp.run(t=t, mode=mode)
+        return self.electrode.recorded_potential.shape
